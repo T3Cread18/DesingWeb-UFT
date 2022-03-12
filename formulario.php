4be355ec7ca4d03b6125a48ac01aa3e84a3bd288
Cherryrubim <78938339+Cherryrubim@users.noreply.github.com>
2022-03-12 04:52:16 -0400

    Add files via upload

--- formulario.php
+++ formulario.php
@@ -6,4 +6,29 @@ $edad = $_POST['edad'];
 $genero = $_POST['genero'];
 $bando = $_POST['bando'];
 $estado = $_POST['estado'];
+
+if(!empty($nombre) || !empty($apellido) || !empty($genero) || 
+!empty($edad) || !empty($bando) || !empty($estado) ){
+
+    $host = "localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname = "cuerpos";
+
+    $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+    if(mysqli_connect_error()){
+       die('Error de Conexion'(mysqli_connect_errorno().')'.mysqli_connect_error());
+    }
+    else{
+        $INSERT="INSERT INTO personas(nombre,apellido,edad,genero,bando,estado)
+        values(?,?,?,?,?,?)";
+
+        $stmt = $conn->prepare($INSERT);
+        $stmt ->bind_param("ssisss", $nombre, $apellido, $edad, $genero, $bando, $estado);
+        $stmt ->execute();
+        echo "DATOS AGREGADOS CON EXITO.";
+    }
+
+}
+
 ?>
