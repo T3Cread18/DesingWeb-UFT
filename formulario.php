cd19c371c5091044fec5c9803898a4130650974e
Cherryrubim <78938339+Cherryrubim@users.noreply.github.com>
2022-03-12 04:51:16 -0400

    Add files via upload

--- formulario.php
+++ formulario.php
@@ -1,2 +1,9 @@
 <?php
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$edad = $_POST['edad'];
+$genero = $_POST['genero'];
+$bando = $_POST['bando'];
+$estado = $_POST['estado'];
 ?>
