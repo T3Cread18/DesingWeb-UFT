02f86426a11a8f33348c42b77e773b2002d49b7f
T3Cread18 <pablor10el@gmail.com>
2022-03-13 19:42:28 -0400

    prueba

---
+++ ver_lista.php
@@ -0,0 +1,46 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lista victimas</title>
+    </head>
+    <body>
+            <?php
+                include('php/conectar.php');
+            ?>
+         <table>
+             <tr><th colspan="6">
+                 <h1>Listado de hombres caídos en batalla</h1></th></tr>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Edad</th>
+                <th>Genero</th>
+                <th>Bando</th>
+                <th>Estado</th>
+            </tr>
+
+            <?php
+                $sql="select * from personas";
+                $resultado=mysqli_query($conn,$sql);
+                while($mostrar=mysqli_fetch_array ($resultado))
+                    {
+            ?>
+                    <tr>
+                        <td><?php echo $mostrar['id']?></td>
+                        <td><?php echo $mostrar['nombre']?></td>
+                        <td><?php echo $mostrar['apellido']?></td>
+                        <td><?php echo $mostrar['edad']?></td>
+                        <td><?php echo $mostrar['genero']?></td>
+                        <td><?php echo $mostrar['bando']?></td>
+                        <td><?php echo $mostrar['estado']?></td>
+                        <td><?php echo "<a href='php/recibir.php?id=".$mostrar['id']."&nombre=".$mostrar['nombre']."'>Agregar</a>"?></td>
+                    </tr>
+            <?php
+                    }
+            ?>
+            </table>
+    </body>
+</html>
